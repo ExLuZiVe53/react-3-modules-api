a4c7b46b8bb7b4b5b1895ed2c9fb6b79a0f3f2a8
ExLuZiVe53 <imstudent27@gmail.com>
2023-09-28 20:40:53 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    posts: null,
+    isLoading: false,
+    error: null,
+  };
+
+  fetchAllPosts() {}
+
+  render() {
+    return (
+      <>
+        <ul>
+          <li>
+            <span></span>
+            <h3></h3>
+            <h4></h4>
+            <p></p>
+          </li>
+        </ul>
+      </>
+    );
+  }
+}
